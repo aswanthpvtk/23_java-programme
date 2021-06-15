ff87b0ec838b1baaa50d539a3b605db1a6109db7
aswanthpvtk <paswanthvtk@gmail.com>
2021-06-15 20:09:57 +0530

    compleated

---
+++ User_inputs/Pgm 7/ArrayNoOfElmts.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+
+public class ArrayNoOfElmts{
+    public static void main(String[] args) {
+        int[] arr=new int[]{5,7,2,9,1};
+        System.out.println("The number of elements present in an array : "+arr.length);
+    }
+}
